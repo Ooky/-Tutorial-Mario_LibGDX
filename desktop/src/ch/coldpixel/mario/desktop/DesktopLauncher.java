083f82655940e483734b8b100c9b420cc1a92fcd
Ooky <blki@hotmail.com>
2015-12-25 02:40:16 +0100

    Initial Commit

---
+++ desktop/src/ch/coldpixel/mario/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ch.coldpixel.mario.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ch.coldpixel.mario.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
